9e63a35612a6ab0ee706bd09ab7a790196b05093
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2023-11-18 08:32:17 -0800

    inprogress

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { Routing } from './Compo/Routing/Routing';
 function App() {
   return (
     <div >
+      
       <Routing/>
+      <h1 style={{textAlign:"center"}}>iShop</h1>
      
     </div>
   );
---
+++ src/Pages/HomeCat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeCat() {
+  return (
+    <div>HomeCat</div>
+  )
+}
+
+export default HomeCat

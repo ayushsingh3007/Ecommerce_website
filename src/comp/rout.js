323c1fb05de47a1c72387869a3c0fa844c4a1d85
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-12-26 21:24:38 -0800

    Update rout.js

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Routes, Route} from 'react-router';
+import React from 'react';
+import { Routes, Route } from 'react-router';
 import Home from './home';
 import Shop from './shop';
 import Cart from './cart';
 import Contact from './contact';
 import Login from './login';
 import Register from './singup';
-
-
 import About from './about';
 
 
-const Rout = ({shop, Filter, allcatefilter, addtocart, cart, setCart}) => {
+
+const Rout = ({ shop, Filter, allcatefilter }) => {
+  
+
   return (
     <>
-    <Routes>
-        <Route path='/' element={<Home addtocart={addtocart}/>}/>
-        <Route path='/cart' element={<Cart cart={cart} setCart ={setCart}/>} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Register/>}/>
-        
-        <Route path='/shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart}/>} />
-        <Route path='/about' element={<Contact />} />
-        <Route path="/contact" element={<About/>}/>
-        
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart  />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+    
 
-         
-    </Routes>
+        {/* Remove addtoCart prop from the Shop route */}
+        <Route path='/shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} />} />
+        <Route path='/about' element={<Contact />} />
+        <Route path="/contact" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default Rout;

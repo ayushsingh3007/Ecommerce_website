e4769a3c4f28792140e90a72ca3fd2f790f67861
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-12-26 21:01:54 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-
+import store from './comp/Redux/Store';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-   
-    
-  </React.StrictMode>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
